0f1cdadaa7d04df3386988af8a66b9dad8778b75
idhun90 <idhun90@naver.com>
2022-10-11 18:14:11 +0900

    Initial Commit

---
+++ Remote Notification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remote Notification
+//
+//  Created by 이도헌 on 2022/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
